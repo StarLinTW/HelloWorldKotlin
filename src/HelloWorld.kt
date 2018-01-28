9abc10324749f7a5c2a118067fa60d5637a26175
StarLinTW <two006621@gmail.com>
2018-01-28 13:58:36 +0800

    first commit

---
+++ src/HelloWorld.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>){
+    var sayHello = "hello welcome!"
+    println("some one says = $sayHello")
+    var response = readLine()
+    println("you response: $response")
+
+}
