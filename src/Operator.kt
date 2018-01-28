5ec029d4f5308f2cadaee55fd1e7917094df78a5
StarLinTW <two006621@gmail.com>
2018-01-28 15:26:11 +0800

    second commit

---
+++ src/Operator.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>){
+    var i = 10.0
+    var j = 20
+    var k = 50
+    println("result = ${i+j}")
+    println("division == ${i/j}")  //must declare type be dobule
+    println("use toFloat = ${k.toFloat()}")
+    println("++k = ${++k}")
+    println("k++ = ${k++}")
+    println("k = $k")
+
+}
